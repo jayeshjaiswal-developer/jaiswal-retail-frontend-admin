7f3dabf3773ace53ca395c3b604f01a88842df6e
jayeshdeveloper <jayesh.developer546@gmail.com>
2026-04-17 17:56:31 +0530

    april26-commit

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,3 +1,3 @@
-// exports.backendBaseUrl = 'http://localhost:8000';
+exports.backendBaseUrl = 'http://localhost:8000';
 
-exports.backendBaseUrl = 'https://server-jaiswal-retail-production.up.railway.app';
+// exports.backendBaseUrl = 'https://server-jaiswal-retail-production.up.railway.app';
